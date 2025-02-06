8241e2545d4abfff93a7b17ad8fa1d039cdbe856
Battlebee12 <Sarab4space@gmail.com>
2025-02-05 20:30:10 -0800

    Gutters working

---
+++ Assets/Scripts/Gutter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Gutter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider triggeredBody)
+    {
+        Debug.Log("detected collisions");
+        Debug.Log($"{triggeredBody.gameObject.name} collided with {gameObject.name} and stopped moving.");
+
+        // Get the Rigidbody of the ball
+        Rigidbody ballRigidBody = triggeredBody.gameObject.GetComponent<Rigidbody>();
+
+        // Store the velocity magnitude before resetting the velocity
+        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+
+        // Reset the linear and angular velocity
+        ballRigidBody.linearVelocity = Vector3.zero;
+        ballRigidBody.angularVelocity = Vector3.zero;
+
+        // Was facing problems with the applying force to z axis, as the colliders got messed up whenever
+        // i changed the direction to z axis
+        Vector3 newVelocity = -Vector3.forward * velocityMagnitude;
+        ballRigidBody.linearVelocity = newVelocity;
+    }
+}
